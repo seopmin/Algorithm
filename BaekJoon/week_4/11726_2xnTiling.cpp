3f149b76690cbcc5a7532859a945cfd4e56b2a1f
pbit <alstjqrhkr2018@gmail.com>
2023-01-19 13:53:46 +0900

    11726 complete

---
+++ BaekJoon/week_4/11726_2xnTiling.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int dp[1001]={0,1,2,};
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+  int n; cin >> n;
+  for(int i=3; i<=n; i++)
+    dp[i] = dp[i-1] + dp[i-2];
+
+  cout << dp[n];
+}
