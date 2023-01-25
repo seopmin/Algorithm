44e16823e58db84dc73db2f4d1d252bb3ef9b559
pbit <alstjqrhkr2018@gmail.com>
2023-01-25 13:09:28 +0900

    update

--- BaekJoon/week_4/2618_경찰차.cpp
+++ BaekJoon/week_4/2618_경찰차.cpp
@@ -1,87 +1,71 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<cstring>
 using namespace std;
 int n, w; 
-vector<pair<int, int>> v;
+vector<pair<int, int>> loc;
 vector<int> result;
-int dp[1001][3]={};
+int dp[1001][1001]={};
 
-int min_func(int d, pair<int, int> p1, pair<int, int> p2) {
-  if(d>=w) return 0;
-  if(dp[d][1]&&dp[d][2]) return min(dp[d][1], dp[d][2]);
+int Find_Dist(int x, int y, int xx, int yy) { return abs(xx - x) + abs(yy - y); }
 
+int func(int p1, int p2) {
+  if(p1==w || p2==w) return 0;
+  if(dp[p1][p2] != -1) return dp[p1][p2];
 
-  // int a = min_func(h+1, {v[h].first, v[h].second}, p2)+abs(p1.first-v[h].first) + abs(p1.second-v[h].second);
-  // int b = min_func(h+1, p1, {v[h].first, v[h].second})+abs(p2.first-v[h].first) + abs(p2.second-v[h].second);
-
-  // if(a <= b){
-  //   dp[h] = a;
-  // } else {
-  //   dp[h] = b;
-  // }
-  // dp[h] = min(a, b);
-  // return dp[h];
-
-  // int a = abs(p1.first-v[h].first) + abs(p1.second-v[h].second);
-  // int b = abs(p2.first-v[h].first) + abs(p2.second-v[h].second);
-  // if(!dp[a][b])
-  //   dp[a][b] = min_func(a, b, {v[a].first, v[a].second}, p2)+abs(p1.first-v[a].first) + abs(p1.second-v[a].second);
+  int next = max(p1, p2) + 1;
   
-  // if(!dp[a][b])
-  //   dp[a][b] = min_func(a, b, p1, {v[b].first, v[b].second})+abs(p2.first-v[b].first) + abs(p2.second-v[b].second);
-
-  int a = min_func(d+1, {v[d].first, v[d].second}, p2)+abs(p1.first-v[d].first) + abs(p1.second-v[d].second);
-  int b = min_func(d+1, p1, {v[d].first, v[d].second})+abs(p2.first-v[d].first) + abs(p2.second-v[d].second);
+  int a, b;
+  a = (p1==0) ? (abs(1-loc[next].first)+abs(1-loc[next].second)) : (abs(loc[p1].first-loc[next].first)+abs(loc[p1].second-loc[next].second));
   
-  if(a < b) {
-    result[d]=1;
-    return a;
+  b = (p2==0) ? (abs(n-loc[next].first)+abs(n-loc[next].second)) : (abs(loc[p2].first-loc[next].first)+abs(loc[p2].second-loc[next].second));
+ 
+
+  int result1 = func(p1, next) + b;
+  int result2 = func(next, p2) + a;
+  if(result1 < result2){
+    result[next-1]=2;
+    dp[p1][p2] = result1;
   } else {
-    result[d]=2;
-    return b;
+    result[next-1]=1;
+    dp[p1][p2] = result2;
   }
-
-  // if(dp[a][b] < dp[a][b]){
-  //   result[a] = 1;
-  //   return dp[a][b];
-  // } else {
-  //   result[b] = 2;
-  //   return dp[a][b];
-  // }
-  
-
-  //dp[h] = min(min_func(h+1, {v[h].first, v[h].second}, p2)+abs(p1.first-v[h].first) + abs(p1.second-v[h].second), min_func(h+1, p1, {v[h].first, v[h].second})+abs(p2.first-v[h].first) + abs(p2.second-v[h].second));
-  
-
-  // int a = abs(p1.first-v[h].first) + abs(p1.second-v[h].second);
-  // int b = abs(p2.first-v[h].first) + abs(p2.second-v[h].second);
-
-  // dp[h] = min(min_func(h+1, {v[h].first, v[h].second}, p2)+a, min_func(h+1, p1, {v[h].first, v[h].second})+b);
-
-  // return dp[h];
-
-
-
-  // int a = abs(p1.first-x) + abs(p1.second-y);
-  // int b = abs(p2.first-x) + abs(p2.second-y);
-  // if(a < b) {
-
-  // }
+  return dp[p1][p2];
 }
+
 int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
   cin >> n >> w;
   pair<int, int> p1={1,1}, p2{n,n};
 
+  loc.push_back({0,0});
   for(int i=0; i<w; i++) {
     int x, y; cin >> x >> y;
-    v.push_back({x, y});
+    loc.push_back({x, y});
     result.push_back(0);
   }
 
-  cout << min_func(0, p1, p2) << endl;
+  memset(dp, -1, sizeof(dp));
+
+  cout << func(0, 0) << endl;
   for(int i=0; i<w; i++) {
     cout << result[i] << endl;
   }
   
 }
+
+
+  // if(d>=w) return 0;
+  // if(dp[d][1] && dp[d][2]) return min(dp[d][1], dp[d][2]);
+
+  // int a = min_func(d+1, {v[d].first, v[d].second}, p2)+abs(p1.first-v[d].first) + abs(p1.second-v[d].second);
+  // int b = min_func(d+1, p1, {v[d].first, v[d].second})+abs(p2.first-v[d].first) + abs(p2.second-v[d].second);
+  
+  // if(a < b) {
+  //   result[d]=1;
+  //   return a;
+  // } else {
+  //   result[d]=2;
+  //   return b;
+  // }
