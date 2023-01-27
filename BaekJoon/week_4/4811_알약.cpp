50e21e6b9946176ad4d56065bc27e5ecec5cbd7b
pbit <alstjqrhkr2018@gmail.com>
2023-01-27 12:53:01 +0900

    2661 좋은 수열

--- BaekJoon/week_4/4811_알약.cpp
+++ BaekJoon/week_4/4811_알약.cpp
@@ -5,7 +5,7 @@ long long dp[31][31];
 long long func(int w, int h) {
   if (w==0 && h==0) return 1;
   if(dp[w][h]) return dp[w][h];
-  if(w>0) dp[w][h] += func(w-1, h+1);
+  if(w>0) dp[w][h] = func(w-1, h+1);
   if(h>0) dp[w][h] += func(w, h-1);
   return dp[w][h];
 } 
