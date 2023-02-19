1bdf128e73bd671f78514e9c47a0ddfc0577ce29
pbit <alstjqrhkr2018@gmail.com>
2023-02-19 11:02:27 +0900

    Modify 2240.cpp

--- BaekJoon/DP/2240.cpp
+++ BaekJoon/DP/2240.cpp
@@ -1,18 +1,1 @@
-// #include<iostream>
-// #include<algorithm>
-// using namespace std;
-// int arr[1001];
-// int dp[1001][2][31];
-// int t, w;
-
-// int func(int idx, int tree, int cnt) {
-//   if(cnt==0) return 
-// }
-
-// int main() {
-//   cin >> t >> w;
-//   for(int i=0; i<t; i++) {
-//     cin >> arr[i];
-//   }
-//   cout << max(func(0, 0, w), func(0, 1, w-1));
-// }
+//
