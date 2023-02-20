940f4cf6f94611e07ccc58ed9fabb6fd41c3b531
pbit <alstjqrhkr2018@gmail.com>
2023-02-20 10:45:50 +0900

    Solved 2133 타일채우기

---
+++ BaekJoon/DP/2133.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int n, DP[31]={0,3}, A[31]={0,1}, B[31]={1,0};
+int main() {
+  cin >> n;
+  for(int i=2; i<n; i++) {
+    A[i]=B[i-1];
+    B[i]=DP[i-1]+A[i-1];
+    DP[i]=DP[i-2]+2*A[i];
+  }
+  
+  cout << DP[n-1];
+}
