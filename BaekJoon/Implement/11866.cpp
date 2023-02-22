b4fb51d6933522b5eaa3d2b23768739a633a3910
pbit <alstjqrhkr2018@gmail.com>
2023-02-22 22:32:59 +0900

    Solved 11866 요세푸스 문제 0

---
+++ BaekJoon/Implement/11866.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+  int n, k; cin >> n >> k;
+
+  vector<int> v;
+  for(int i=1; i<=n; i++) {
+    v.push_back(i);
+  }
+  int idx = k-1;
+
+  cout << "<";
+  while(v.size()!=1) {
+    while(idx >= v.size()) {
+      idx -= v.size();
+    }
+    cout << v[idx] << ", ";
+    v.erase(v.begin()+idx);
+    idx-=1;
+    idx += k;
+  }
+  cout << v[0] << ">";
+}
