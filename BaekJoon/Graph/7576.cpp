ceb27c0e9c10654dc1a9365b7df281eb1d3c3c51
pbit <alstjqrhkr2018@gmail.com>
2023-02-24 06:54:13 +0900

    Solved 7576 토마토

--- BaekJoon/Graph/7576.cpp
+++ BaekJoon/Graph/7576.cpp
@@ -6,45 +6,24 @@ using namespace std;
 int n, m, map[1001][1001];
 int dx[] = {0,0,1,-1};
 int dy[] = {1,-1,0,0};
+queue<pair<int,int>> q;
 
-int check() {
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<m; j++) {
-      if(map[i][j]==0) return 0;
-    }
-  }
-  return 1;
-}
-
-int bfs() {
-  queue<tuple<int,int,int>> q;
-  int result=0, depth=0;
-
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<m; j++) {
-      if(map[i][j]==1) {
-        q.push({j, i, depth});
-      }
-    }
-  }
+void bfs() {
 
   while(q.size()) {
-    int x, y; tie(x, y, depth) = q.front(); q.pop();
-    if(check()) return result;
+    
+    int x, y; tie(x, y) = q.front(); q.pop();
     for(int i=0; i<4; i++) {
       int nx = x+dx[i];
       int ny = y+dy[i];
 
       if(nx>=0 && ny>=0 && nx<m && ny<n && map[ny][nx]==0) {
-        map[ny][nx]=1;
-        q.push({nx, ny, depth+1});
-        result=max(result, depth+1);
+        map[ny][nx]= map[y][x]+1;
+        q.push({nx, ny});
       }
     } 
     
   }
-
-  return -1;
 }
 
 int main() {
@@ -53,65 +32,23 @@ int main() {
   for(int i=0; i<n; i++) {
     for(int j=0; j<m; j++) {
       cin >> map[i][j];
+      if(map[i][j]==1){
+        q.push({j,i});
+      }
     }
   }
   
-  cout << bfs();
+  bfs();
+
+  int sum=0;
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<m; j++) {
+      if(map[i][j]==0) {
+        cout << -1;
+        return 0;
+      }
+      sum = max(sum, map[i][j]);
+    }
+  }
+  cout << sum-1;
 }
-
-
-// #include<iostream>
-// #include<queue>
-// #include<tuple>
-// #include<algorithm>
-// using namespace std;
-// int n, m, map[1001][1001];
-// int dx[] = {0,0,1,-1};
-// int dy[] = {1,-1,0,0};
-// queue<pair<int,int>> q;
-
-
-// void bfs() {
-
-//   while(q.size()) {
-    
-//     int x, y; tie(x, y) = q.front(); q.pop();
-//     for(int i=0; i<4; i++) {
-//       int nx = x+dx[i];
-//       int ny = y+dy[i];
-
-//       if(nx>=0 && ny>=0 && nx<m && ny<n && map[ny][nx]==0) {
-//         map[ny][nx]= map[y][x]+1;
-//         q.push({nx, ny});
-//       }
-//     } 
-    
-//   }
-// }
-
-// int main() {
-//   cin >> m >> n;
-
-//   for(int i=0; i<n; i++) {
-//     for(int j=0; j<m; j++) {
-//       cin >> map[i][j];
-//       if(map[i][j]==1){
-//         q.push({j,i});
-//       }
-//     }
-//   }
-  
-//   bfs();
-
-//   int sum=0;
-//   for(int i=0; i<n; i++) {
-//     for(int j=0; j<m; j++) {
-//       if(map[i][j]==0) {
-//         cout << -1;
-//         return 0;
-//       }
-//       sum = max(sum, map[i][j]);
-//     }
-//   }
-//   cout << sum-1;
-// }
