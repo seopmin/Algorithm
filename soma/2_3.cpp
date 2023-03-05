cbbbe789e6f82a29507b4d16ca7273cbb8027b66
pbit <alstjqrhkr2018@gmail.com>
2023-03-05 14:11:53 +0900

    Solved Soma problem

--- soma/2_3.cpp
+++ soma/2_3.cpp
@@ -1,3 +1,25 @@
+/*
+
+4 5 5
+1 0
+5 1
+6 1
+10 0
+17 0  
+
+
+12 25 8
+7 1
+25 0
+26 0
+27 0
+28 0
+29 0
+30 0
+31 0
+
+*/
+
 // n: 회전문의 칸 개수
 // k: 한번 회전할 때 지속되는 시간(초)
 #include<iostream>
@@ -10,20 +32,28 @@ vector<long long> solve(long long n, long long k, vector<vector<long long>> v) {
   long long real_k = k;
   int i = 1;
   vector<int> pos;
+  pos.push_back(n/2);
   long long last_time = -1;
   int w=0;
   while(time) {
-    cout << 1 << endl;
+    
     if(last_time == -1) {  // 배열의 잘못된 인덱스 접근을 막기 위해
       if(v[i][0]==time) {
         if(i==v.size()-1) {
           last_time=time+k;
         }
+        cout << 1 << endl;
         pos.push_back(n/2);
         i++;
+
+        if(real_k<=0) {
+          real_k=k;
+        }
       }
     }
-    cout << 2 << endl;
+
+    time++;
+    real_k--;
     if(real_k >= 0) {
       for(int j=w; j<pos.size(); j++) {
         pos[j]--;
@@ -35,14 +65,14 @@ vector<long long> solve(long long n, long long k, vector<vector<long long>> v) {
         }
       }
     }
-    cout << 3 << endl;
     if(last_time==time) {
       cout << 4 << endl;
       break;
-    }
-    
-    real_k--;
-    time++;
+    }    
+  }
+
+  for(int i=w; i<pos.size(); i++) {
+    result.push_back(-1);
   }
 
   cout << result.size() << endl;
