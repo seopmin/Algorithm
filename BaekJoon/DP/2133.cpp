ab3a3ecc4aa894932fc865175873017b6c56a336
pbit <alstjqrhkr2018@gmail.com>
2023-04-17 14:20:27 +0900

    Modify 2133.cpp

--- BaekJoon/DP/2133.cpp
+++ BaekJoon/DP/2133.cpp
@@ -4,8 +4,9 @@ int n, DP[31]={0,3}, A[31]={0,1}, B[31]={1,0};
 int main() {
   cin >> n;
   for(int i=2; i<n; i++) {
-    A[i]=B[i-1];
-    B[i]=DP[i-1]+A[i-1];
+    //A[i]=B[i-1];
+    //B[i]=DP[i-1]+A[i-1];
+    A[i]=DP[i-2]+A[i-2];
     DP[i]=DP[i-2]+2*A[i];
   }
   
