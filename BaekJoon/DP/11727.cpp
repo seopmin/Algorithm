3ecbc0415d0f8fe71386a1915d2697f11a01eea0
pbit <alstjqrhkr2018@gmail.com>
2023-04-18 16:00:19 +0900

    Solved 11727.cpp (DP)

---
+++ BaekJoon/DP/11727.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+  int n; cin >> n;
+  vector<int> dp{0,1,3};
+  for(int i=3; i<=n; i++)
+    dp.push_back((dp[i-1]+2*dp[i-2])%10007);
+  cout << dp[n];
+}
