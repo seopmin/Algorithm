429c1add9eb881c65da675c613cdb0085e63d4cc
pbit <alstjqrhkr2018@gmail.com>
2023-05-06 12:10:17 +0900

    Modify 1655.cpp(Data Structures

--- BaekJoon/Data Structures/1655.cpp
+++ BaekJoon/Data Structures/1655.cpp
@@ -1,28 +1,23 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+priority_queue<int , vector<int>, greater<int>> min_heap;
+priority_queue<int, vector<int>, less<int>> max_heap;
 
 int main() {
   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-  int n, k; cin >> n;
-  priority_queue<int, vector<int>, less<int>> max_heap;
-  priority_queue<int, vector<int>, greater<int>> min_heap;
-
+  int n; cin >> n;
   for(int i=0; i<n; i++) {
-    cin >> k;
-    if(max_heap.size() == min_heap.size()) {
+    int k; cin >> k;
+    if(max_heap.size()<=min_heap.size())
       max_heap.push(k);
-    } else if(max_heap.size() < min_heap.size()) {
-      max_heap.push(k);
-    } else {
+    else
       min_heap.push(k);
-    }
     
-    if(!max_heap.empty() && !min_heap.empty() && max_heap.top() > min_heap.top()) {
-      int a=max_heap.top(); max_heap.pop();
-      int b=min_heap.top(); min_heap.pop();
-      max_heap.push(b);
-      min_heap.push(a);
+    if(max_heap.size() && min_heap.size() && max_heap.top()>min_heap.top()) {
+      int a = max_heap.top(); max_heap.pop();
+      int b = min_heap.top(); min_heap.pop();
+      max_heap.push(b); min_heap.push(a);
     }
     cout << max_heap.top() << '\n';
   }
