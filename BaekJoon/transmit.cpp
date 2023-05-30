7c7877a2ae29c942753a7a4e358f148a9760c3ee
pbit <alstjqrhkr2018@gmail.com>
2023-05-30 11:12:32 +0900

    Solving 15684.cpp

--- BaekJoon/transmit.cpp
+++ BaekJoon/transmit.cpp
@@ -1,26 +1,29 @@
 #include<iostream>
-#include<vector>
-#include<cmath>
 using namespace std;
-vector<int> v;
-vector<int> result[10];
+int n, m, h, visited[11][31], a, b;
 
-void func(int l, int r, int depth) {
-  int m = (l+r)/2;
-  if(l==m) { result[depth].push_back(v[m]); return; }
-  result[depth].push_back(v[m]);
-  func(l, m-1, depth+1);
-  func(m+1, r, depth+1);
+
+int check() {
+  for(int i=1; i<=h; i++) {
+    int pos=i;
+    for(int j=0; j<n; j++) {
+      if(visited[i][j]) pos++;
+    }
+  }
+}
+
+void func(int pos, int cnt) {
+  if(cnt>3) return;
+  if(check()) {
+
+  }
 }
 
 int main() {
-  int n, k; cin >> n;
-  for(int i=0; i<pow(2, n)-1; i++) { cin >> k; v.push_back(k); }
-
-  func(0, pow(2, n)-2, 0);
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<result[i].size(); j++)
-      cout << result[i][j] << ' ';
-    cout << endl;
+  cin >> n >> m >> h;
+  for(int i=0; i<m; i++) {
+    cin >> a >> b;
+    visited[a][b]=1;
   }
+  func(1, 0);
 }
