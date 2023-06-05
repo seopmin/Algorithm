e7666933f052efc0169cb734c099e0802360f1c5
pbit <alstjqrhkr2018@gmail.com>
2023-06-05 10:32:14 +0900

    Solved 1202.cpp(Greedy)

--- BaekJoon/Greedy/1202.cpp
+++ BaekJoon/Greedy/1202.cpp
@@ -1,44 +1,37 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 #include<algorithm>
 using namespace std;
-vector<pair<int, int>> v1;
-vector<int> v2;
-int n, k, a, b;
+int n, k, M, V, C;
+vector<pair<int, int>> nv;
+vector<int> kv;
+priority_queue<int, vector<int>, less<int>> q;
 
 bool comp(pair<int, int> a, pair<int, int> b) {
-  return (a.first < b.first && a.second > b.second);
+	return a.first<b.first;
 }
 
 int main() {
-  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-  cin >> n >> k;
-  for(int i=0; i<n; i++) {
-    cin >> a >> b;
-    v1.push_back({a, b});
-  }
-  sort(v1.begin(), v1.end(), comp);
+	cin >> n >> k;
+	for(int i=0; i<n; i++) {
+		cin >> M >> V; nv.push_back({M, V});
+	}
+	for(int i=0; i<k; i++) {
+		cin >> C; kv.push_back(C);
+	}
 
-  for(int i=0; i<k; i++) {
-    cin >> a;
-    v2.push_back(a);
-  }
-  sort(v2.begin(), v2.end());
-
-  long long sum=0;
-  int idx=0;
-  for(int i=0; i<k; i++) {
-    while(v2[i])
-  }
-  
-  // for(int i=0; i<k; i++) {
-  //   for(int j=0; j<v1.size(); j++) {
-  //     if(v2[i] > v1[j].first) {
-  //       sum += v1[j].second;
-  //       v1.erase(v1.begin()+j);
-  //     } 
-  //   }
-  // }
-
-  cout << sum;
+	sort(nv.begin(), nv.end(), comp);
+	sort(kv.begin(), kv.end());
+	long long result=0;
+	for(int i=0, j=0; i<kv.size(); i++) {
+		while(j<nv.size() && kv[i]>=nv[j].first) {
+			q.push(nv[j].second);
+			j++;
+		}
+		if(q.size()) {
+			result += q.top(); q.pop();
+		}
+	}
+	cout << result;
 }
