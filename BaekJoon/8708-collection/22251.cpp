20dbba004d543c7635c3b8a72ac50d1231f46fa0
seopmin <alstjqrhkr2018@gmail.com>
2024-06-03 22:09:08 +0900

    22251 빌런 호석

--- BaekJoon/8708-collection/22251.cpp
+++ BaekJoon/8708-collection/22251.cpp
@@ -4,43 +4,36 @@ using namespace std;
 
 int n, k, p, x;
 
-// typedef struct {
-//   int m_arr[7];
-// } number;
-
-// number v[10];
-
-int g_arr[10][7];
-
-bool checkArr(int a[], int b[], int size) {
-  for (int i=0; i<size; i++) {
-    if (a[i] != b[i]) return false;
-  }
-  return true;
-}
-
-void dfs(int arr[], int depth) {
-  
-  if (depth > p) return;
-
-  
-  for (int i=0; i<=n; i++) {
-
-  }
-}
+int arr[10][7] = {
+    {1, 1, 1, 1, 1, 1, 0},  // 숫자 0
+    {0, 1, 1, 0, 0, 0, 0},  // 숫자 1
+    {1, 1, 0, 1, 1, 0, 1},  // 숫자 2
+    {1, 1, 1, 1, 0, 0, 1},  // 숫자 3
+    {0, 1, 1, 0, 0, 1, 1},  // 숫자 4
+    {1, 0, 1, 1, 0, 1, 1},  // 숫자 5
+    {1, 0, 1, 1, 1, 1, 1},  // 숫자 6
+    {1, 1, 1, 0, 0, 0, 0},  // 숫자 7
+    {1, 1, 1, 1, 1, 1, 1},  // 숫자 8
+    {1, 1, 1, 1, 0, 1, 1}   // 숫자 9
+  };
 
 int main() {
   cin >> n >> k >> p >> x;
 
-  g_arr[0] = {1, 1, 1, 1, 1, 1, 0};  // 숫자 0
-  g_arr[1] = {0, 1, 1, 0, 0, 0, 0};  // 숫자 1
-  g_arr[2] = {1, 1, 0, 1, 1, 0, 1};  // 숫자 2
-  g_arr[3] = {1, 1, 1, 1, 0, 0, 1};  // 숫자 3
-  g_arr[4] = {0, 1, 1, 0, 0, 1, 1};  // 숫자 4
-  g_arr[5] = {1, 0, 1, 1, 0, 1, 1};  // 숫자 5
-  g_arr[6] = {1, 0, 1, 1, 1, 1, 1};  // 숫자 6
-  g_arr[7] = {1, 1, 1, 0, 0, 1, 0};  // 숫자 7
-  g_arr[8] = {1, 1, 1, 1, 1, 1, 1};  // 숫자 8
-  g_arr[9] = {1, 1, 1, 1, 0, 1, 1};  // 숫자 9
+  int result=0;
+  for (int i=1; i<=n; i++) {
+    if (i==x) continue;  // x와 같을 경우 반전이 없기 때문에 continue;
+    int cnt = 0;
+    int start = x, end = i;
 
+    for (int j=0; j<k; j++) {  // 디스플레이의 수만큼 반복
+      for (int z=0; z<7; z++) {  // 각 숫자를 비교하여 반전해야하는 LED 수를 카운트 함
+        if (arr[start%10][z]!=arr[end%10][z]) cnt++;
+      }
+      start /= 10;
+      end /= 10;
+    }
+    if (cnt <= p) result++; 
+  }
+  cout << result;
 }
